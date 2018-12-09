9549e495ba686ecd54b3b5cda7331326105b89de
Arthur <arthur@Arthurs-MacBook-Air.local>
2018-12-09 13:44:27 +0300

    Initiate project

--- Cell English/ViewController.swift
+++ Cell English/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    var myTableView : UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    var animalArray : [String] = ["Dog", "Cat", "Bear", "Bat", "Otter"]
+    var cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.white
+        
+        myTableView.frame = view.frame
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        view.addSubview(myTableView)
+        
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: cellID)
+        cell?.textLabel?.text = animalArray[indexPath.row]
+        return cell!
+    }
+    
 }
 
